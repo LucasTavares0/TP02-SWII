3935a5b6d2b8f27b0ddd2e921602c43392720e83
LucasTavares0 <lucas.tavares@modalgr.com.br>
2019-09-30 22:12:34 -0300

    Adding Bootstrap dependencies and updating _Layout.cshtm

---
+++ TP02/TP02/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace TP02
+{
+    public class BundleConfig
+    {
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear();
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapjs").Include(
+                        "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrapcss").Include(
+                        "~/Content/bootstrap.min.css",
+                        "~/Content/bootstrap-responsive.min.css"));
+        }
+    }
+}
